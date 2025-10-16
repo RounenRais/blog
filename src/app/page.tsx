b0485b0cb228e4a5f83596b4bbb06e0c6a9799a2
Rais <aksu3796@gmail.com>
2025-10-16 15:06:19 +0300

    fix profile  error

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,14 +1,14 @@
 import React from "react";
 import { Badge } from "@/components/ui/badge";
-import {Profile} from "../components/ui/profile";
+import Profile from "../components/ui/profile";
 import  Work  from "../components/work";
 export default function Home() {
   return (
     <div className=" items-center w-full justify-center flex  flex-col gap-6 ">
       <Badge className=" p-3">
-        Hello, I'm an indie app developer based from Turkiye!
+        Hello, I&apos;m an indie app developer based from Turkiye!
       </Badge>
-      <Profile/>
+      <Profile></Profile>
       <Work></Work>
 
     </div>
